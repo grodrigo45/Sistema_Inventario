0103f526e2d6a981c12505e8a009ff2cb2b39b68
Rodrigo Gonzalez <grodrigo45@gmail.com>
2021-11-27 10:32:23 -0600

    Primer Commit

---
+++ vistas/noacceso.php
@@ -0,0 +1,28 @@
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <div class="content-wrapper">
+    <!-- Main content -->
+    <section class="content">
+
+      <!-- Default box -->
+      <div class="row">
+        <div class="col-md-12">
+      <div class="box">
+<div class="text-center box-header with-border">
+  <h1 class="">Sin acceso</h1>
+  <h2 class=" ">Vuelva a intentarlo mas tarde o comuniquese con el administrador</h2> 
+  <img class="img-fluid img-thumbnail " src="https://cdn-icons.flaticon.com/png/512/2032/premium/2032399.png?token=exp=1636407942~hmac=415b2e874ff6b3f1a627f203ecf6432e"  alt="">
+  <div class="box-tools pull-right">
+
+  </div>
+</div>
+<!--box-header-->
+
+<!--fin centro-->
+      </div>
+      </div>
+      </div>
+      <!-- /.box -->
+
+    </section>
+    <!-- /.content -->
+  </div>
